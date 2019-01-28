c0eb335b7e86f49de8ccd739a8c3a9b336bd81de
christian <rhyuben.starmoon@gmail.com>
2019-01-28 17:30:26 +0700

    admin route update

--- src/api/admin.js
+++ src/api/admin.js
@@ -13,30 +13,35 @@ route.get('/:id',verifyToken, (req, res, next)=>{
     /**
      * get admin detail
      */
+    res.status(200).send("Admin get by id = ");
 });
 
 route.post('/',verifyToken, (req, res, next)=>{
     /**
      * create admin
      */
+    res.status(200).send("Admin created");
 });
 
 route.delete('/',verifyToken, (req, res, next)=>{
     /**
      * delete admin
      */
+    res.status(200).send("Admin deleted");
 });
 
 route.put('/',verifyToken, (req, res, next)=>{
     /**
      * update admin
      */
+    res.status(200).send("Admin updated");
 });
 
 route.put('/picture/:id',verifyToken, (req, res, next)=>{
     /**
      * update admin picture
      */
+    res.status(200).send("Admin picture updated");
 });
 
 module.exports = route;
