940fdd5b0fc0b0810a7063bd5c5b3da219a4f513
rubenkristian <rhyuben.starmoon@gmail.com>
2019-02-28 23:11:50 +0700

    model

--- model/student/index.js
+++ model/student/index.js
@@ -1,66 +1,148 @@
 const connection = require('../connection');
 const util = require('../../util');
 
-class Student{
-    InsertStudent({firstname, secondname, born, bornplace, fathername, mothername, address, photo}, callback){
-        connection.query(`INSERT INTO ${table}() VALUES(?,?,?,?,?,?,?,?,?)`, [firstname, secondname, born, bornplace, fathername, mothername, address, photo, util.getDateNow()], (err, results)=>{
-            if(err){
-                console.error(err);
-                throw err;
-            }
-            callback(true);
-        });
+function createStudent({nis, name, gender, religion, born_place, born_date, father_name, mother_name, address, phone_number, class_id}, callback){
+    let result = {
+        status: 0,
+        err: "Terjadi kesalahan, NIS yang dimasukan sudah terdaftar sebelumnya"
     }
-
-    UpdateStudent({id, firstname, secondname, born, bornplace, fathername, mothername, address}, callback){
-        connection.query(`UPDATE ${table} SET firstname = ?, secondname = ?, born = ?, bornplace = ?, fathername = ?, mothername = ?, address = ?, dateupdate = ? WHERE id = ?`, [firstname, secondname, born, bornplace, fathername, mothername, address, util.getDateNow(), id], (err, results)=>{
-            if(err){
-                console.error(err);
-                throw err;
+    connection.execute(`SELECT * FROM tbl_student WHERE nis = ?`, [nis], (err, res, field)=>{
+        if(err){
+            result = {
+                status: -1,
+                err: "Terjadi kesalahan dalam server"
             }
-            callback(results.affectedRows);
-        });
-    }
-
-    UpdateStudentPhoto({id, photo}, callback){
-        connection.query(`UPDATE ${table} SET picture = ?, dateupdate = ? WHERE id = ?`, [photo, util.getDateNow(), id], (err, results)=>{
-            if(err){
-                console.error(err);
-                throw err;
-            }
-            callback(results.affectedRows);
-        });
-    }
-
-    DeleteStudent({id}, callback){
-        connection.query(`DELETE FROM ${table} WHERE id IN(?)`, [id], (err, results)=>{
-            if(err){
-                console.error(err);
-                throw err;
-            }
-            callback(results.affectedRows);
-        });
-    }
-    
-    StudentList({search, sortby, sort, index, rows}, callback){
-        if(search.trim().length > 0){
-            connection.query(`SELECT * FROM ${table} WHERE firstname = ? OR secondname = ? OR born = ? OR bornplace = ? OR fathername = ? OR mothername = ? OR address = ? ORDER BY ${sortby} ${sort} LIMIT ${index},${rows}`, [search,search,search,search,search,search,search], (err, results, fields)=>{
-                if(err){
-                    console.error(err);
-                    throw err;
-                }
-                callback(results, fields);
-            });
+            callback(result);
         }else{
-            connection.query(`SELECT * FROM ${table}ORDER BY ${sortby} ${sort} LIMIT ${index},${rows}`, (err, results, fields)=>{
-                if(err){
-                    console.error(err);
-                    throw err;
-                }
-                callback(results, fields);
-            });
+            if(res.length > 0){
+                callback(result);
+            }else{
+                let val = [nis, name, gender, religion, born_place, born_date, father_name, mother_name, address, phone_number, class_id, util.getDateNow()];
+                connection.execute(`INSERT INTO tbl_student(NIS, name, gender, religion, born_place, born_date, father_name, mother_name, address, phone_number, class_id, datecreated)`, val, (err, res)=>{
+                    if(err){
+                        result = {
+                            status: -1,
+                            err: "Terjadi kesalahan dalam server"
+                        }
+                        callback(result);
+                    }else{
+                        if(res.affectedRows > 0){
+                            result = {
+                                status: 1,
+                                err: null
+                            }
+                            callback(result);
+                        }else{
+                            result = {
+                                status: 0,
+                                err: "Terjadi kesalahan, Data tidak berhasil di simpan"
+                            }
+                            callback(result);
+                        }
+                    }
+                });
+            }
         }
+    });
+}
+
+function updateClass({id, class_id}, callback){
+    let result = {
+        status: 0,
+        err: "Terjadi kesalahan, gagal memperbaharui"
+    }
+    connection.execute(`UPDATE tbl_student SET class_id = ? WHERE student_id = ?`, [class_id, id], (err, res)=>{
+        if(err){
+            result = {
+                status: -1,
+                err: "Terjadi kesalahan dalam server"
+            }
+            callback(result);
+        }else{
+            if(res.affectedRows > 0){
+                result = {
+                    status: 1,
+                    err: null
+                }
+                callback(result);
+            }else{
+                callback(result);
+            }
+        }
+    });
+}
+
+function updateStudent({id, name, gender, religion, born_place, born_date, father_name, mother_name, address, phone_number}, callback){
+    let val = [name, gender, religion, born_place, born_date, father_name, mother_name, address, phone_number, id];
+    let result = {
+        status: 0,
+        err: "Terjadi kesalahan, gagal memperbaharui"
+    }
+    connection.execute(`UPDATE tbl_student SET name = ?, gender = ?, religion = ?, born_place = ?, born_date = ?, father_name = ?, mother_name = ?, address = ?, phone_number = ? WHERE student_id = ?`, val, (err, res)=>{
+        if(err){
+            result = {
+                status: -1,
+                err: "Terjadi kesalahan dalam server"
+            }
+            callback(result);
+        }else{
+            if(res.affectedRows > 0){
+                result = {
+                    status: 1,
+                    err: null
+                }
+                callback(result);
+            }else{
+                callback(result);
+            }
+        }
+    });
+}
+
+function deleteStudent({id}, callback){
+    let result = {
+        status: 0,
+        err: "Terjadi kesalahan, gagal menghapus"
+    }
+    connection.execute(`DELETE FROM tbl_student WHERE student_id IN(?)`, [id], (err, res)=>{
+        if(err){
+            result = {
+                status: -1,
+                err: "Terjadi kesalahan dalam server"
+            }
+            callback(result);
+        }else{
+            if(res.affectedRows > 0){
+                result = {
+                    status: 1,
+                    err: null
+                }
+                callback(result);
+            }else{
+                callback(result);
+            }
+        }
+    });
+}
+
+function listStudent({search, orderby, order, index, len}, callback){
+    if(search.trim().length > 0){
+        let src = `%${search}%`;
+        connection.execute(`SELECT * FROM tbl_student WHERE name LIKE N? OR religion LIKE N? OR born_place LIKE N? OR father_name LIKE N? OR mother_name LIKE N? OR address LIKE N? OR phone_number LIKE N? ORDER BY ${orderby} ${order} LIMIT ?,?`, [src,src,src,src,src,src,src,index,len], callback);
+    }else{
+        connection.execute(`SELECT * FROM tbl_student ORDER BY ${orderby} ${order} LIMIT ?,?`, [index, len], callback);
     }
 }
 
-module.exports = Student;
+function getStudent({id}, callback){
+    connection.execute(`SELECT * FROM tbl_student WHERE student_id = ?`, [id], callback);
+}
+
+module.exports = {
+    createStudent,
+    updateClass,
+    updateStudent,
+    deleteStudent,
+    listStudent,
+    getStudent
+};
--- model/teacher/index.js
+++ model/teacher/index.js
@@ -1,66 +1,135 @@
 const connection = require('../connection');
 const util = require('../../util');
 
-class Teacher{
-    createTeacher({firstname, lastname, email, phonenumber, address, dateborn, placeborn, picture, status}, callback){
-        connection.query(`INSERT INTO ${table}() VALUES(?,?,?,?,?,?,?,?,?,?)`, [firstname, lastname, email, phonenumber, address, dateborn, placeborn, picture, status, util.getDateNow()], (err, results)=>{
-            if(err){
-                console.error(err);
-                throw err;
-            }
-            callback(true);
-        });
+function createTeacher({nip, name, gender, religion, born_place, born_date, address, phone_number, relationship}, callback){
+    let result = {
+        status: 0,
+        err: "Terjadi kesalahan, tidak berhasil menyimpan"
     }
-
-    updateTeacher({id, firstname, lastname, email, phonenumber, address, dateborn, placeborn, status}, callback){
-        connection.query(`UPDATE ${table} SET firstname = ?, lastname = ?, email = ?, phonenumber = ?, address = ?, dateborn = ?, placeborn = ?, status = ?, dateupdate = ? WHERE id = ?`, [firstname, lastname, email, phonenumber, address, dateborn, placeborn, status, util.getDateNow(), id], (err, results)=>{
-            if(err){
-                console.error(err);
-                throw err;
+    connection.execute(`SELECT * FROM tbl_teacher WHERE NIP = ?`, [nip], (err, res, field)=>{
+        if(err){
+            result = {
+                status: -1,
+                err: "Terjadi kesalahan dalam server"
             }
-            callback(results.affectedRows);
-        });
-    }
-
-    updateTeacherPicture({id, picture}, callback){
-        connection.query(`UPDATE ${table} SET picture = ? dateupdate = ? WHERE id = ?`, [picture, util.getDateNow(), id], (err, results)=>{
-            if(err){
-                console.error(err);
-                throw err;
-            }
-            callback(results.affectedRows);
-        });
-    }
-
-    deleteTeacher({id}, callback){
-        connection.query(`DELETE FROM ${table} WHERE id IN(?)`, id, (err, results)=>{
-            if(err){
-                console.error(err);
-                throw err;
-            }
-            callback(results.affectedRows);
-        });
-    }
-
-    teacherList({search, orderby, order, index, rows}, callback){
-        if(search.trim().length > 0){
-            connection.query(`SELECT * FROM ${table} WHERE firstname = ? OR lastname = ? OR address = ? OR phonenumber = ? OR placeborn = ? OR dateborn = ? OR email = ? ORDER BY ${orderby} ${order} LIMIT ${index},${rows}`, [search, search, search, search, search, search, search], (err, results, fields)=>{
-                if(err){
-                    console.error(err);
-                    throw err;
-                }
-                callback(results, fields);
-            });
+            callback(result);
         }else{
-            connection.query(`SELECT * FROM ${table} ORDER BY ${orderby} ${order} LIMIT ${index},${rows}`, (err, results, fields)=>{
-                if(err){
-                    console.error(err);
-                    throw err;
+            if(res.length > 0){
+                result = {
+                    status: 0,
+                    err: "NIP telah terdaftar"
                 }
-                callback(results, fields);
-            });
+                callback(result);
+            }else{
+                let val = [nip, name, gender, religion, born_place, born_date, address, phone_number, relationship, util.getDateNow()];
+                connection.execute(`INSERT INTO tbl_teacher(NIP, name, gender, religion, born_place, born_date, address, phone_number, relationship, datecreated) VALUES(?,?,?,?,?,?,?,?,?,?)`,val, (err, res)=>{
+                    if(err){
+                        result = {
+                            status: -1,
+                            err: "Terjadi kesalahan dalam server"
+                        }
+                        callback(result);
+                    }else{
+                        if(res.affectedRows > 0){
+                            result = {
+                                status: 1,
+                                err: null
+                            }
+                            callback(result);
+                        }else{
+                            callback(result);
+                        }
+                    }
+                })
+            }
         }
+    });
+}
+
+function updateTeacher({id, nip, name, religion, born_place, born_date, address, phone_number, relationship}, callback){
+    let result = {
+        status: 0,
+        err: "Terjadi kesalahan, tidak berhasil menyimpan"
+    }
+    connection.execute(`SELECT * FROM tbl_teacher WHERE NIP = ?`, [nip], (err, res, field)=>{
+        if(err){
+            result = {
+                status: -1,
+                err: "Terjadi kesalahan dalam server"
+            }
+            callback(result);
+        }else{
+            if(res.length > 0){
+                result = {
+                    status: 0,
+                    err: "NIP telah terdaftar"
+                }
+                callback(result);
+            }else{
+                let val = [nip, name, gender, religion, born_place, born_date, address, phone_number, relationship, id];
+                connection.execute(`UPDATE tbl_teacher SET NIP = ?, name = ?, religion = ?, born_place = ?, born_date = ?, address = ?, phonenumber = ?, relationship = ? WHERE teacher_id = ?`,val, (err, res)=>{
+                    if(err){
+                        result = {
+                            status: -1,
+                            err: "Terjadi kesalahan dalam server"
+                        }
+                        callback(result);
+                    }else{
+                        if(res.affectedRows > 0){
+                            result = {
+                                status: 1,
+                                err: null
+                            }
+                            callback(result);
+                        }else{
+                            callback(result);
+                        }
+                    }
+                })
+            }
+        }
+    });
+}
+
+function deleteTeacher({id}, callback){
+    let result = {
+        status: 0,
+        err: "Terjadi kesalahan, gagal menghapus"
+    }
+    connection.execute(`DELETE FROM tbl_teacher WHERE teacher_id IN(?)`, [id], (err, res)=>{
+        if(err){
+            result = {
+                status: -1,
+                err: "Terjadi kesalahan dalam server"
+            }
+            callback(result);
+        }else{
+            if(res.affectedRows > 0){
+                result = {
+                    status: 0,
+                    err: null
+                }
+                callback(result);
+            }else{
+                callback(result);
+            }
+        }
+    });
+}
+
+function listTeacher({search, orderby, order, index, len}, callback){
+    if(search.trim().length > 0){
+        let src = `%${search.trim()}%`;
+        connection.execute(`SELECT * FROM tbl_teacher WHERE NIP LIKE N? OR name LIKE N? OR born_place LIKE N? OR address LIKE N? OR phone_number LIKE N? ORDER BY ${orderby} ${order} LIMIT ?,?`,[src, src, src, src, src, index, len], callback);
+    }else{
+        connection.execute(`SELECT * FROM tbl_teacher ORDER BY ${orderby} ${order} LIMIT ?,?`, [index, len], callback);
     }
 }
 
-module.exports = Teacher;
+
+module.exports = {
+    createTeacher,
+    updateTeacher,
+    deleteTeacher,
+    listTeacher
+};
